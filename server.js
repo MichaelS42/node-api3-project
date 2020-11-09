2b35763a57327f046495ab2a61d8d391d9321b18
Michael Sanchez <michaelsanchez42@live.com>
2020-11-08 17:19:24 -0700

    finished middleware

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-const express = require('express');
-
+const express = require("express");
+const helmet = require("helmet");
 const server = express();
+const userRouter = require("./users/userRouter.js");
 
-server.get('/', (req, res) => {
+server.use(express.json());
+server.use(helmet());
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${
+      req.headers.origin
+    }`
+  );
+  next();
+}
+
+server.use(logger);
+
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "server is online" });
+});
 
-//custom middleware
+function errorHandler(error, req, res, next) {
+  res.status(error.status).json(error);
+}
 
-function logger(req, res, next) {}
+server.use(errorHandler);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,82 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+const Users = require("./userDb.js");
+const Posts = require("../posts/postDb.js")
 
-router.post('/', (req, res) => {
+router.post("/", validateUser, (req, res) => {
   // do your magic!
+  res.json({".post response": ""})
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", validatePost, (req, res) => {
   // do your magic!
+  res.json({".post response": "" })
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
+  res.json({".get ": "/"})
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", validateUserId, (req, res) => {
   // do your magic!
+  res.json({ ".get response": "" });
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
+  res.json({".get response": "id/posts"})
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
+  res.json({".delete response": ""})
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", validateUserId, (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // do your magic!
+  const user = await Users.getById(req.params.id);
+  console.log(user);
+  if (!user) {
+    next({ status: 404, message: "user doesnt exist" });
+    return;
+  }
+  next();
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  console.log(req.body)
+  if(!Object.keys(req.body).length) {
+    next({ status: 400, message: "missing user data"});
+    return
+  }
+  if(!req.body.name) {
+    next({ status: 400, message: "missing required name field"});
+    return
+  }
+  next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  console.log(req.body)
+  if (!Object.keys(req.body).length) {
+    next({ status: 400, message: "missing post data"});
+    return
+  } 
+  if (!req.body.text) {
+    next({ status: 400, message: "missing required text field"});
+    return
+  }
+  next();
 }
 
 module.exports = router;
